3524ea1af958cd0d433799a96c13218f198ad3c8
pengdonglai <pengdonglai@bytedance.com>
2023-08-27 23:54:03 +0800

    feat: add esbuild to project

---
+++ etsc.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  // Supports all esbuild.build options
+  esbuild: {
+    bundle: true,
+    platform: 'node',
+    external: ['node_modules'],
+    define: {
+      'process.env.NODE_ENV': '"production"'
+    },
+    plugins: [
+      {
+        name: 'externalize-node-modules',
+        setup(build) {
+          build.onResolve({ filter: /^node_modules\// }, args => {
+            return {
+              path: args.path,
+              namespace: 'external'
+            };
+          });
+          build.onLoad({ filter: /^node_modules\// }, args => {
+            if (args.path.endsWith('.js')) {
+              return {
+                contents: `import '${args.path}';`,
+                loader: 'js'
+              };
+            }
+          });
+        }
+      }
+    ]
+  },
+};
